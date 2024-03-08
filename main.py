aafc19dc6cb88042ac9aef7128a5c5521b5ca1df
katsuyuki.i <katsuyukiikeda142857@gmail.com>
2024-03-07 21:44:07 -0800

    Test the Cell class

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from graphics import Window, Point, Line
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    p1 = Point(100, 100)
-    p2 = Point(500, 500)
-    line1 = Line(p1, p2)
-    win.draw_line(line1, "black")
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(200, 200, 300, 300)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(225, 225, 275, 275)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(400, 400, 500, 500)
+
     win.wait_for_close()
 
 
